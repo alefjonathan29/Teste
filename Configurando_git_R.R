e5350d251dea37dcf04fab8635d62c1c984a5977
Alef Jonathan <a.lef93@hotmail.com>
2023-07-09 00:10:29 -0300

    asdasd

---
+++ Configurando_git_R.R
@@ -0,0 +1,7 @@
+#digitar no terminal do R
+
+#git config --global user.name 'Fernando da Silva'
+#git config --global user.email 'fernando@email.com'
+
+usethis::create_github_token()
+gitcreds::gitcreds_set()
